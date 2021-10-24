0033081617282c9f213a460027f83670a5a718e6
alokaryan <alokaryan@gmail.com>
2021-10-24 14:56:05 +0530

    Update k8s-node.sh

--- k8s-node.sh
+++ k8s-node.sh
@@ -33,7 +33,7 @@ echo
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce=5:19.03.14~3-0~ubuntu-bionic docker-ce-cli=5:19.03.14~3-0~ubuntu-bionic 
+sudo apt-get install -y docker-ce=5:19.03.14~3-0~ubuntu-bionic docker-ce-cli=5:19.03.14~3-0~ubuntu-bionic 
 
 ## check this link https://stackoverflow.com/questions/27657888/how-to-install-docker-specific-version
 echo
