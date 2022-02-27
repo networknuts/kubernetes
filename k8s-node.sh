6e8b969d69170302f84f17b7855304e43a846b87
alokaryan <alokaryan@gmail.com>
2022-02-27 13:56:49 +0530

    updated script for k8s nodes

--- k8s-node.sh
+++ k8s-node.sh
@@ -14,40 +14,40 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
 
-sysctl --system
+sudo sysctl --system
 echo  '\033[1mInstalling Docker\033[0m'
-apt-get update
+sudo apt-get update
 
-apt-get install ca-certificates curl gnupg lsb-release -y
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt-get update
+sudo apt-get update
 
-apt-get install -y docker-ce=5:19.03.15~3-0~ubuntu-focal docker-ce-cli=5:19.03.15~3-0~ubuntu-focal containerd.io
+sudo apt-get install -y docker-ce=5:19.03.15~3-0~ubuntu-focal docker-ce-cli=5:19.03.15~3-0~ubuntu-focal containerd.io
 
 echo  '\033[1mInstalling Kubeadm, Kubelet and Kubectl\033[0m'
 apt-get update
 
-apt-get install -y apt-transport-https ca-certificates curl
+sudo apt-get install -y apt-transport-https ca-certificates curl
 
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-apt-get update
+sudo apt-get update
 
-apt-get install -y kubelet=1.19.10-00 kubeadm=1.19.10-00 kubectl=1.19.10-00
+sudo apt-get install -y kubelet=1.19.10-00 kubeadm=1.19.10-00 kubectl=1.19.10-00
 
-apt-mark hold kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 
 ## Installing openssh-server
-echo 
+echo
 echo  '\033[7mInstalling Openssh-server & starting sshd\033[0m'
 sudo apt install -y openssh-server
-systemctl start sshd
+sudo systemctl start sshd
 echo
 
 echo
@@ -55,8 +55,8 @@ echo  '\033[7mScript finished. You need to join node to cluster\033[0m'
 echo  '\033[7mRun on master node to get join token - \033[0m'
 echo
 echo  '\033[1mkubeadm token create --print-join-command\033[0m'
-echo 
-echo 
+echo
+echo
 echo  '\033[5mkubeadm token create --print-join-command\033[0m'
 echo
-echo 
+echo
