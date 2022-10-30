8643c04dfa253d63e7ad6b01e21e5a386a2533b4
alokaryan <alokaryan@gmail.com>
2022-10-30 15:05:18 +0530

    kubernetes manager 1.24.4-00

--- k8s-manager-1-24.1-00.sh
+++ k8s-manager-1-24.1-00.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y kubelet=1.24.1-00 kubeadm=1.24.1-00 kubectl=1.24.1-00 docker.io
+apt-get install -y kubelet=1.24.4-00 kubeadm=1.24.4-00 kubectl=1.24.4-00 docker.io
 apt-mark hold kubelet kubeadm kubectl
 cat <<EOF | sudo tee /etc/docker/daemon.json
 {
