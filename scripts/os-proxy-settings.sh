b5b04b39a9390fcb55e9b4fd67c8d02408c32662
alokaryan <alokaryan@gmail.com>
2023-07-04 14:06:30 +0530

    proxy-settings-all-machines

--- scripts/os-proxy-settings.sh
+++ scripts/os-proxy-settings.sh
@@ -18,16 +18,23 @@ export NO_PROXY="127.0.0.1,localhost"
 EOF
 
 chmod +x /etc/profile.d/proxy.sh
+sh /etc/profile.d/proxy.sh
 source /etc/profile.d/proxy.sh
 
-echo "configure proxy for wget"
+#echo "configure proxy for wget"
 
-cat <<EOF | sudo tee ~/.wgetrc
-use_proxy = on
-http_proxy = http://10.0.0.200:3128/ 
-https_proxy = http://10.0.0.200:3128/ 
-ftp_proxy = http://10.0.0.200:3128/ 
+#cat <<EOF | sudo tee ~/.wgetrc
+#use_proxy = on
+#http_proxy = http://10.0.0.200:3128/ 
+#https_proxy = http://10.0.0.200:3128/ 
+#ftp_proxy = http://10.0.0.200:3128/ 
 
-EOF
+#EOF
 
-source ~/.wgetrc
+#source ~/.wgetrc
+
+echo "SETUP YOUR BASH ENVIRONMENT"
+echo -e "\n\n#PROXY SETTINGS\nexport http_proxy=http://10.0.0.200:3128\nexport https_proxy=http://10.0.0.200:3128\nexport no_proxy=localhost,127.0.0.1,10.0.0.100,10.0.0.1,10.0.0.2,172.16.0.0/16" >> ~/.bashrc
+source ~/.bashrc
+
+
