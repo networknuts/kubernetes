62328a0b683e1b6a8627d305846e153d1192daee
alokaryan <alokaryan@gmail.com>
2023-07-12 12:57:48 +0530

    Update os-proxy-settings.sh

--- scripts/os-proxy-settings.sh
+++ scripts/os-proxy-settings.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 #### RUN ON ALL MACHINES of CLUSTER ####
 ##### ONLY IF YOU ARE USING PROXY ######
-
+clear
+echo
+echo
+echo "********"
 echo "configuring proxy for OS"
-
+echo "********"
+echo 
+echo
+sleep 5
 cat <<EOF | sudo tee /etc/profile.d/proxy.sh
 
 #!/bin/bash
 export http_proxy="http://10.0.0.200:3128/"
 export https_proxy="http://10.0.0.200:3128/"
 export ftp_proxy="http://10.0.0.200:3128/"
-export no_proxy="127.0.0.1,localhost,10.0.0.0/8,10.0.0.100,10.0.0.1,10.0.0.2"
+export no_proxy="127.0.0.1,localhost,10.0.0.0/8,10.0.0.100,10.0.0.1,10.0.0.2,172.16.0.0/16"
 
 # For curl
 export HTTP_PROXY="http://10.0.0.200:3128/"
 export HTTPS_PROXY="http://10.0.0.200:3128/"
 export FTP_PROXY="http://10.0.0.200:3128/"
-export NO_PROXY="127.0.0.1,localhost,10.0.0.0/8,10.0.0.100,10.0.0.1,10.0.0.2"
+export NO_PROXY="127.0.0.1,localhost,10.0.0.0/8,10.0.0.100,10.0.0.1,10.0.0.2,172.16.0.0/16"
 
 
 EOF
@@ -25,17 +31,7 @@ chmod +x /etc/profile.d/proxy.sh
 sh /etc/profile.d/proxy.sh
 source /etc/profile.d/proxy.sh
 
-#echo "configure proxy for wget"
 
-#cat <<EOF | sudo tee ~/.wgetrc
-#use_proxy = on
-#http_proxy = http://10.0.0.200:3128/ 
-#https_proxy = http://10.0.0.200:3128/ 
-#ftp_proxy = http://10.0.0.200:3128/ 
-
-#EOF
-
-#source ~/.wgetrc
 
 echo "SETUP YOUR BASH ENVIRONMENT"
 echo -e "\n\n#PROXY SETTINGS\nexport http_proxy=http://10.0.0.200:3128\nexport https_proxy=http://10.0.0.200:3128\nexport no_proxy=localhost,127.0.0.1,10.0.0.100,10.0.0.1,10.0.0.2,172.16.0.0/16" >> ~/.bashrc
